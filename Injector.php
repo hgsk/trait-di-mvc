4432ba408fb3e3f53c832b5a294bbedaade295c1
il_r-hagiwara <r-hagiwara@infiniteloop.co.jp>
2014-02-27 14:07:49 +0900

    設計方針をコメント

--- Injector.php
+++ Injector.php
@@ -13,6 +13,7 @@
 trait BaseInjector
 {
 	public static function prepare($name=null,$instance){
+		// TODO 例外処理を書く
 		if(!isset($name)){
 			$name = uniqid();
 		}
--- controller-sample.php
+++ controller-sample.php
@@ -1,12 +1,17 @@
 <?php
 require 'Container.php';
 require 'Injector.php';
+
+// TODO static makeでテンプレートを呼び出す
+// TODO none-static setでテンプレート変数を渡す
 class View {
 	public function show($str){
 		echo "<p>$str</p>";
 	}
 }
 
+// TODO 各ビジネスモデルはModelを継承するようにする
+// TODO Modelはgetter,setter,validationのみを提供する
 class User{
 	public function get(){
 		return [
@@ -15,7 +20,15 @@ class User{
 		];
 	}
 }
+// TODO DataMapperを定義する
+// TODO 複数のDataMapperにまたがるトランザクションを実装できるようにする
+// http://www.sitepoint.com/forums/showthread.php?593227-DataMapper-Pattern-and-Transactions
+// For the first method, you could handle transactions inside each mapper.
+// Each mapper checks if a transaction is already started, and if not, starts one itself.
+// If a transaction is already started, then it just goes about its business. 
 
+// TODO Decoratorに変更して、View用のModel処理を記述する cf:Draper
+// viewで、$user->getName()
 class Navigation{
 	use ViewInjector;
 	public function show(){
@@ -23,6 +36,8 @@ class Navigation{
 	}
 }
 
+// TODO Context(DCI)に変更して、DataMapper間の処理を記述する
+// TODO Context内でModelにRoleを与えて処理する -> Model内で動的にtraitをuseできるようにしたい $user->use('role')->roleInteraction() <-むりっぽい
 class Content{
 	use ViewInjector;
 	use ModelInjector;
@@ -31,6 +46,7 @@ class Content{
 		$this->title = $title;
 	}
 	public function show(){
+	//	$users = UserMapper::find(1);
 		$users = $this->getModel('user')->get();
 		$this->getView()->show($this->title);
 		foreach($users as $user){
@@ -39,6 +55,7 @@ class Content{
 	}
 }
 
+// TODO 抽象クラス BaseControllerに共通処理と、個別処理のインターフェースを書く
 class PageController {
 	use NavigationInjector;
 	use ContentInjector;
