99e23982fa855f00b6cc42cc994e1893c2b1c677
il_r-hagiwara <r-hagiwara@infiniteloop.co.jp>
2014-02-27 14:08:05 +0900

    簡易テストを追加

--- controller-sample.php
+++ controller-sample.php
@@ -72,5 +72,16 @@ class Application {
 	}
 }
 
+
 $app = new Application;
 $app->run();
+
+//////////Test//////////
+class Test{
+	public function testCamelize(){
+		echo camelize('hoge')=='Hoge' ? 'passed' : 'failed';
+		//echo camelize('hOge')=='Hoge' ? 'passed' : 'failed';
+		//echo camelize('hoGe')=='Hoge' ? 'passed' : 'failed';
+		//echo camelize('hogE')=='Hoge' ? 'passed' : 'failed';
+	}
+}
