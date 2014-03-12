cbf0c8b0d76cd3c46f8f37b1cba2a6492192a2c6
hgsk <hgsk85@gmail.com>
2014-03-12 13:57:06 +0900

    ROOT URIへのルーティングを定義できるようにした

--- src/app/Application.php
+++ src/app/Application.php
@@ -3,15 +3,18 @@
  * Application
  */
 class Application {
+	const ROOT_CONTROLLER_NAME = 'Welcome';
 	use \framework\ControllerInjector;
 	public function run(){
 		// /Controller/Action/param1/value1/param2/value2/...
 		$requestURI = explode('/', $_SERVER['REQUEST_URI']);
 
-		if(!empty($requestURI[2])){
+		if(!empty($requestURI[1])){
 			$this->getController($requestURI[1])->$requestURI[2](array_slice($requestURI,3));
-		}else{
+		}elseif(!empty($requestURI[2])){
 			$this->getController($requestURI[1])->index();
+		}else{
+			$this->getController(self::ROOT_CONTROLLER_NAME)->index();
 		}
 	}
 }
